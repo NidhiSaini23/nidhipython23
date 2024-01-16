7aec967f546281fcde7e73b6e3e3bbd20965ef26
NidhiSaini23 <151614756+NidhiSaini23@users.noreply.github.com>
2024-01-16 15:11:44 +0530

    Add files via upload

---
+++ nidhi.py
@@ -0,0 +1,18 @@
+#simpleint.py
+p=float(input("Enter the principal amount:"))
+r=float(input("Enetr the rate of interest:"))
+t=float(input("Enetr the time:"))
+si=p*t*r/100
+amtpay=p+si
+#displaytehresult
+print("result")
+print("-"*50)
+print("enter the value of principal:",p)
+print("enter the value of rate:",r)
+print("enter the value of time:",r)
+print(si)
+print("total amount to be paid=",amtpay)
+print("-"*50)
+a=50
+b=10
+print("value of a{} and value of b{}".format(a,b))
